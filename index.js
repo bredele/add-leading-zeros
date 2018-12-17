da2f434101378f3ec6a8ee4a648f35c69f40c683
bredele <olivier.wietrich@gmail.com>
2018-12-17 14:42:00 -0700

    Add documentation

--- index.js
+++ index.js
@@ -1,13 +1,18 @@
 
+/**
+ * Add leading zeros.
+ *
+ * @param {String} str
+ * @param {Number?} grouping
+ * @api public
+ */
 
-module.exports = (str, group = 4) => {
+module.exports = (str, grouping = 4) => {
   const length = str.length
-  const total = Math.ceil(length / group) * group
+  const total = Math.ceil(length / grouping) * grouping
   if (total !== length) {
     var l = total - length
-    while (l--) {
-      str = '0' + str
-    }
+    while (l--) str = '0' + str
   }
   return str
 }
